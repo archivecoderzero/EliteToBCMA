fa859bc2ebfd93034478daa7e9749f8c49345001
js7 <archivecoderzero@gmail.com>
2020-10-03 19:18:02 -0700

    initial UI content , no formating , initial plan, no JS

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 
 var express = require("express");
+require('jquery')
 var app = express();
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
