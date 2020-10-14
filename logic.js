b96da602a2f10c54f8934e02bf2721646887190f
js7 <archivecoderzero@gmail.com>
2020-10-13 19:49:49 -0700

    added else

--- logic.js
+++ logic.js
@@ -1,23 +1,28 @@
-function submit ( ) {
+function submit() {
     event.preventDefault();
     barcode = $("#barcode").val().trim();
     let resultDiv = $("<li>");
     let linkable = $("h5");
-    var n = barcode.includes("]")
-
-    if(n){
-        ner = barcode.slice(6,99);
-    var the =  ner ;
-    linkable.append(the)
-    $("#results").append(resultDiv);
-    resultDiv.append(linkable);
+    var n = barcode.includes("]C1")
+    if (n) {
+        ner = barcode.slice(3, 99);
+        linkable.append(ner)
+        $("#results").append(resultDiv);
+        resultDiv.append(linkable);
+    }
+    else{
+        linkable.append("Not a Valid Barcode")
+        $("#results").append(resultDiv);
+        resultDiv.append(linkable);
+    }
 }
-}
 
-function clear ( ) {
+function clear() {
     event.preventDefault();
-    $( "#results" ).empty();
+    $("#results").empty();
 }
+
+
 
 
 $(document).on("click", "#submit", submit);
