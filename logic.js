429b67cbf4dd179d16ddc27e77f6684866a931d5
js7 <archivecoderzero@gmail.com>
2020-11-12 22:37:17 -0800

    variation trim code

--- logic.js
+++ logic.js
@@ -1,34 +1,25 @@
 function submit() {
     event.preventDefault();
     barcode = $("#barcode").val().trim();
-    let resultDiv = $("h5");
     let linkable = $("h5");
     var n = barcode.includes("]C1")
     let convertedBc = barcode.slice(3, 99);
-
+    let resDiv = 
     
     if (n) {
         linkable.append(convertedBc)
-        $("#results").append(resultDiv);
-        resultDiv.append(linkable);
-
     }
     else{
-        linkable.append("Not a Valid Barcode")
-        $("#results").append(resultDiv);
-        resultDiv.append(linkable);
-
+        linkable.replaceWith("Not a Valid Barcode")
     }
 }
 
-function clear() {
-    event.preventDefault();
-    $("#results").empty();
+
+function eraser() {
+    return p1 * p2; 
 }
-
-
 
 
 $(document).on("click", "#submit", submit);
 
-$(document).on("click", "#clear", clear);
+$(document).on("click", "#clear", reload());
